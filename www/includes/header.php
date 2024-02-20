05ce403d0e258db24af08c3a149217e0d9a48826
MagicMaan <38400400+Magicmaan@users.noreply.github.com>
2024-02-20 21:29:13 +0000

    stuff

--- www/includes/header.php
+++ www/includes/header.php
@@ -1,14 +1,28 @@
-<div id="header">
-	<div id="icon-container">
-		<img id="icon" href="../images/icon.png">
-	</div>
 
-	<div id="bar-container">
-		<ul>
-			<li><a href="home.php">Home</a></li>
-			<li><a href="dashboard.php">Dashboard</a></li>
-			<li><a href="default.asp">Login</a></li>
-			<li><a href="default.asp">Sign up</a></li>
-		</ul>
-	</div>
-</div>
+        <div id="header-container">
+            <div id="header">
+				<div id="logo-container">
+					<img id="icon" src="../icon.png">
+					<img id="name" src="./images/landing/icon.png">
+				</div>
+				<div id="buttons-container">
+					<a id="button">Home</a>
+					<?php
+						session_start();
+						$_SESSION["isLoggedIn"] = false;
+						if (isset($_SESSION["isLoggedIn"]) && $_SESSION["isLoggedIn"]) {
+							echo '<a id="button">Dashboard</a>';
+						} else {
+							echo '<a id="button">Enrol</a>';
+							echo '<a id="button">Login</a>';
+						}
+					?>
+                	
+					
+					
+					<div id="button">Contact</div>
+					<div id="button">Contact</div>
+                
+				</div>
+            </div>
+        </div>

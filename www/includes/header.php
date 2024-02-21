de14885845fc20b4a8aa6a32fc9d4c1738c795b8
MagicMaan <38400400+Magicmaan@users.noreply.github.com>
2024-02-21 00:38:34 +0000

    uh

--- www/includes/header.php
+++ www/includes/header.php
@@ -3,14 +3,14 @@
             <div id="header">
 				<div id="logo-container">
 					<img id="icon" src="../icon.png">
-					<img id="name" src="./images/landing/icon.png">
+					<p id="name">APERTURE</p>
 				</div>
 				<div id="buttons-container">
 					<a id="button">Home</a>
 					<?php
 						session_start();
-						$_SESSION["isLoggedIn"] = false;
-						if (isset($_SESSION["isLoggedIn"]) && $_SESSION["isLoggedIn"]) {
+						$_SESSION["isLoggedIn"] = false; //dev shit
+						if (isset($_SESSION["isLoggedIn"]) && $_SESSION["isLoggedIn"]) { //if logged in do this else that 
 							echo '<a id="button">Dashboard</a>';
 						} else {
 							echo '<a id="button">Enrol</a>';
